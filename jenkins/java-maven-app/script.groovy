2ab171bf34efe86864b6586961a1098a5a2a5365
fabiuslihanda <117307052+lihandafabius@users.noreply.github.com>
2026-04-27 21:47:19 +0100

    Implement Docker image build and deployment steps
    Added Docker image build and push functionality.

--- jenkins/java-maven-app/script.groovy
+++ jenkins/java-maven-app/script.groovy
@@ -1,14 +1,20 @@
 def buildApp() {
     echo "building the application..."
+    sh 'cd jenkins/java-maven-app/ && mvn package'
 }
 return this
 
-def testApp() {
-    echo "testing the application..."
+def buildDockerImage() {
+    echo 'Building the Docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+        sh 'cd jenkins/java-maven-app/ && docker build -t lihanda/demo-app:3.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push lihanda/demo-app:3.0'
+    }
+
 }
 return this
 def deployApp() {
-    echo "deploying the application..."
-    echo "deploying version ${params.VERSION}"
+    echo 'Deploying the application...'
 }
 return this
