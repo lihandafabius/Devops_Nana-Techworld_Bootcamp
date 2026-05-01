63d46df0e9bd3a112d6d5135cbe84aa7ba2276f3
Lihanda <fabiuslihandaachevi@gmail.com>
2026-05-01 21:47:29 +0100

    intergrate shared-lib

--- jenkins/java-maven-app/script.groovy
+++ jenkins/java-maven-app/script.groovy
@@ -1,19 +1,3 @@
-def buildApp() {
-    echo "building the application..."
-    sh 'cd jenkins/java-maven-app/ && mvn package'
-}
-return this
-
-def buildDockerImage() {
-    echo 'Building the Docker image...'
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
-        sh 'cd jenkins/java-maven-app/ && docker build -t lihanda/demo-app:3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push lihanda/demo-app:3.0'
-    }
-
-}
-return this
 def deployApp() {
     echo 'Deploying the application...'
 }
