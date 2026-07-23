0b87236011aa483e8ae337f333ad0d4ea56f3fac
Lihanda <fabiuslihandaachevi@gmail.com>
2026-07-23 20:14:00 +0100

    learn terraorm variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,35 +1,56 @@
 provider "aws" {
   region = "eu-north-1"
   access_key = ""
-  secret_key = ""
+  secret_key = "
 }
  
+# variable "cidr_blocks" {
+#   description = "CIDR blocks for the subnets"
+# #   default = ["10.0.40.0/24"]
+#   type = list(string)
+# }
+
+# variable "cidr_blocks" {
+#   description = "CIDR block for the VPC"
+# #   default = ["10.0.0.0/16"]
+#   type = list(string)
+# } 
+
+variable "cidr_blocks" {
+  description = "CIDR blocks for the vpc and subnets"
+  type = list(object({
+    cidr_block = string
+    name       = string
+  }))
+}
+
+
+variable "environment" {
+  description = "development environment"
+  default = "development"
+}
+
 resource "aws_vpc" "development_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr_blocks[1].cidr_block
     tags = {
-        Name = "development_vpc"
+        Name = var.cidr_blocks[1].name
     }
 
 }
 
 resource "aws_subnet" "development_subnet1" {
   vpc_id     = aws_vpc.development_vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.cidr_blocks[0].cidr_block
   availability_zone = "eu-north-1a"
     tags = {
-        Name = "development_subnet1"
+        Name = var.cidr_blocks[0].name
     }
 }
 
-data "aws_vpc" "existing_vpc" {
-    default = true
+output "dev_vpc_id" {
+  value = aws_vpc.development_vpc.id
 }
 
-resource "aws_subnet" "development_subnet2" {
-  vpc_id     = data.aws_vpc.existing_vpc.id
-  cidr_block = "172.31.48.0/20"
-  availability_zone = "eu-north-1a"
-    tags = {
-        Name = "development_subnet2"
-    }
+output "dev_subnet1_id" {
+  value = aws_subnet.development_subnet1.id
 }
