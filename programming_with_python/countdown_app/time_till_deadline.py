6b28b998e0ad30fbe2d3aaafae3412c191e07e8c
Lihanda <fabiuslihandaachevi@gmail.com>
2026-08-21 12:23:00 +0100

    add date validation

--- programming_with_python/countdown_app/time_till_deadline.py
+++ programming_with_python/countdown_app/time_till_deadline.py
@@ -1,10 +1,19 @@
 from datetime import datetime
+
+
 def calculate_time_till_deadline(checked_date):
     start_time = datetime.today()
     end_time = checked_date
 
     time_till_deadline = end_time - start_time
     return time_till_deadline.days
+
+def validate_date(deadline):
+
+        checked_date = datetime.strptime(deadline, "%d/%m/%Y")
+        return checked_date
+
+
 
 user_input = input("Please enter your goal and deadline for your project:")
 input_list = user_input.split(":")
@@ -14,10 +23,19 @@ user_input_dictionary = {
 }
 goal = user_input_dictionary["goal"]
 deadline = user_input_dictionary["deadline"]
-checked_date = datetime.strptime(deadline, "%d/%m/%Y")
-days = calculate_time_till_deadline(checked_date)
+while True:
+    try:
 
-print(f"You have {days} days remaining to {goal}")
+        valid_date = validate_date(deadline)
+        days = calculate_time_till_deadline(valid_date)
+        print(f"You have {days} days remaining to {goal}")
+        break
+
+    except ValueError:
+        print("Please enter the date in the correct format: DD/MM/YYYY")
+        deadline = input("Enter deadline: ")
+
+
 
 
 
