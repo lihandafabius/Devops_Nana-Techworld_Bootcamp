35e07ef00f7f38ead3fe7923e61acaa3470fee5f
Lihanda <fabiuslihandaachevi@gmail.com>
2026-09-21 18:32:22 +0100

    add  deploy app server and db server playbook. tough day!

--- ansible-exercises/src/test/java/AppTest.java
+++ ansible-exercises/src/test/java/AppTest.java
@@ -1,27 +1,23 @@
 import com.example.Application;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
+@Component
 public class AppTest {
+
+    @Autowired
+    Connection connection;
 
     @Test
     public void testAppOne() {
-        Application myApp = new Application();
-
-        String result = myApp.getStatus();
-
+        Application myapp = new Application(connection);
+        String result = myapp.getStatus();
         assertEquals("OK", result);
-    }
-
-    @Test
-    public void testAppTwo() {
-        Application myApp = new Application();
-
-        boolean result = myApp.getCondition(true);
-
-        assertTrue(result);
     }
 
 }
